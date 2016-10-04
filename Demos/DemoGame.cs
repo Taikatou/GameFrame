f05f9507e30fa5b244b57442324c17b734dbce84
Taikatou <13123947@studentmail.ul.ie>
2016-10-04 11:37:27 +0100

    Huzzah started game project

---
+++ Demos/DemoGame.cs
@@ -0,0 +1,42 @@
+﻿using GameFrame;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Demos
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class DemoGame : GameFrameGame
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+
+        public DemoGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
