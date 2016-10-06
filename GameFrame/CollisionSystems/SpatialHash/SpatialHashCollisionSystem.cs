03b1bade97b39269f7ca3187ac2f271c54860832
Taikatou <13123947@studentmail.ul.ie>
2016-10-06 17:34:07 +0100

    Simple collision systems

---
+++ GameFrame/CollisionSystems/SpatialHash/SpatialHashCollisionSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace GameFrame.CollisionSystems.SpatialHash
+{
+    public class SpatialHashCollisionSystem<T> : ICollisionSystem
+    {
+        private readonly Dictionary<Point, T> _spatialHash;
+
+        public SpatialHashCollisionSystem()
+        {
+            _spatialHash = new Dictionary<Point, T>();
+        }
+
+        public void AddNode(Point point, T node)
+        {
+            _spatialHash[point] = node;
+        }
+
+        public void RemoveNode(Point point)
+        {
+            _spatialHash.Remove(point);
+        }
+
+        public bool CheckCollision(int x, int y)
+        {
+            var point = new Point(x, y);
+            var found = CheckCollision(point);
+            return found;
+        }
+
+        public bool CheckCollision(Point p)
+        {
+            var found = _spatialHash.ContainsKey(p);
+            return found;
+        }
+    }
+}
