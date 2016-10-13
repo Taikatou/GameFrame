caff143c46c4fc3d08483db0e77936f694ba07cf
Taikatou <13123947@studentmail.ul.ie>
2016-10-13 19:52:00 +0100

    Added eightway movement

---
+++ GameFrame/PathFinding/PossibleMovements/EightWayPossibleMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace GameFrame.PathFinding.PossibleMovements
+{
+    public class EightWayPossibleMovement : FourWayPossibleMovement
+    {
+        public Point[] GetAdjacentLocations(Point fromLocation)
+        {
+            return new[]
+            {
+                new Point(fromLocation.X-1, fromLocation.Y  ),
+                new Point(fromLocation.X,   fromLocation.Y+1),
+                new Point(fromLocation.X+1, fromLocation.Y  ),
+                new Point(fromLocation.X,   fromLocation.Y-1),
+
+                new Point(fromLocation.X-1, fromLocation.Y-1),
+                new Point(fromLocation.X+1, fromLocation.Y-1),
+                new Point(fromLocation.X+1, fromLocation.Y+1),
+                new Point(fromLocation.X-1, fromLocation.Y+1)
+            };
+        }
+    }
+}
