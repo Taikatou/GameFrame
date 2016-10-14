89e938e9739ff8440f5f17a4001923286a6992b0
Taikatou <13123947@studentmail.ul.ie>
2016-10-14 11:57:13 +0100

    Diagonal distance

---
+++ GameFrame/PathFinding/Heuristics/DiagonalDistance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace GameFrame.PathFinding.Heuristics
+{
+    public class DiagonalDistance : IHeuristic
+    {
+        public Int32 GetTraversalCost(Point location, Point otherLocation)
+        {
+            var deltaX = otherLocation.X - location.X;
+            var deltaY = otherLocation.Y - location.Y;
+            return Math.Max(deltaX, deltaY);
+        }
+    }
+}
