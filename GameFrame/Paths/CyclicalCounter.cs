643f2a0326c932140a487777601f99708a0be2fa
Taikatou <13123947@studentmail.ul.ie>
2016-10-14 11:47:47 +0100

    Different paths

---
+++ GameFrame/Paths/CyclicalCounter.cs
@@ -0,0 +1,25 @@
+﻿namespace GameFrame.Paths
+{
+    public class CyclicalCounter
+    {
+        private int _direction;
+        public int CurrentIndex;
+        public readonly int EndIndex;
+
+        public CyclicalCounter(int startIndex, int endIndex, int direction=1)
+        {
+            CurrentIndex = startIndex;
+            EndIndex = endIndex;
+            _direction = direction;
+        }
+
+        public void Increment()
+        {
+            CurrentIndex += _direction;
+            if(CurrentIndex == EndIndex-1 || CurrentIndex == 0)
+            {
+                _direction = -(_direction);
+            }
+        }
+    }
+}
