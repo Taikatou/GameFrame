0a3769a83f458e645e20bf5d0f5a6a6580ca343f
AndrewWare95 <andware@gmail.com>
2016-10-25 18:55:37 +0100

    Debugging back button, Issue with matching position

--- Demos/BackButton.cs
+++ Demos/BackButton.cs
@@ -4,26 +4,25 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.Entities;
+using System.Diagnostics;
 
 namespace Demos
 {
     public class BackButton : IRenderable
     {
         
-        private readonly Texture2D _entityTexture;
+        private readonly Texture2D buttonTexture;
         private Point _tileSize;
         public readonly Entity Entity;
         private readonly ExpiringSpatialHashCollisionSystem<Entity> _spaitalHash;
         public Rectangle FrameRectangle;
         public Vector2 Offset { get; }
 
-        public Vector2 ScreenPosition
+       public Vector2 ScreenPosition
         {
             get
             {
-                var value = Entity.Position * _tileSize.ToVector2();
-                var position = Entity.Position.ToPoint();
-                return value;
+                return new Vector2(50, 30);
             }
         }
 
@@ -33,23 +32,25 @@ namespace Demos
             set { Entity.Position = value; }
         }
 
-        public BackButton(ContentManager content, ExpiringSpatialHashCollisionSystem<Entity> spaitalHash, Entity entity, Point tileSize)
+        public BackButton(ContentManager content)
         {
-            _entityTexture = content.Load<Texture2D>("TopDownRpg/Character");
-            Entity = entity;
-            _tileSize = tileSize;
+            buttonTexture = content.Load<Texture2D>("TopDownRPG/Character");
             Offset = new Vector2(_tileSize.X / 2, _tileSize.Y / 2);
-            _spaitalHash = spaitalHash;
             FrameRectangle = new Rectangle(new Point(), _tileSize);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_entityTexture, ScreenPosition, FrameRectangle, Color.White);
+            spriteBatch.Draw(buttonTexture, ScreenPosition, Color.White);
         }
 
         public bool Hit(Point point)
         {
+            Debug.WriteLine("Screen Position   " + ScreenPosition);
+            if (point.X == 50)
+            {
+                return true;
+            }
             return false;
         }
     }
--- Demos/DemoScreen.cs
+++ Demos/DemoScreen.cs
@@ -8,8 +8,10 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input.Touch;
 using MonoGame.Extended;
+using MonoGame.Extended.Entities;
 using MonoGame.Extended.ViewportAdapters;
 using System;
+using System.Diagnostics;
 
 namespace Demos
 {
@@ -19,6 +21,8 @@ namespace Demos
         public readonly Camera2D Camera;
         public readonly SpriteBatch SpriteBatch;
         private BackButton _backButton;
+        private Entity _entity;
+        private Vector2 _tileSize;
 
         public DemoScreen(ViewportAdapter viewPort, SpriteBatch spriteBatch)
         {
@@ -30,7 +34,9 @@ namespace Demos
         public override void LoadContent()
         {
             base.LoadContent();
-            _backButton = new BackButton();
+            _backButton = new BackButton(Content);
+
+
             var clickController = new ClickController();
             clickController.MouseControl.OnPressedEvent += (state, mouseState) =>
             {
@@ -43,10 +49,12 @@ namespace Demos
             };
             clickController.TouchScreenControl.AddSmartGesture(moveGesture);
             UpdateList.Add(clickController);
+            RenderList.Add(_backButton);
         }
 
         public void CheckHit(Point point)
         {
+            Debug.WriteLine("" + point);
             if (_backButton.Hit(point))
             {
                 Action action = Show<MainMenuScreen>;
