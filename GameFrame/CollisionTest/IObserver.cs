9b1838a4e045c84c5f30f5c7c9aa61b30abdb15a
Taikatou <13123947@studentmail.ul.ie>
2016-11-05 17:02:46 +0000

    Revert "Revert "Pong game""

--- GameFrame/CollisionTest/IObserver.cs
+++ GameFrame/CollisionTest/IObserver.cs
@@ -1,7 +1,11 @@
-﻿namespace GameFrame.CollisionTest
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace GameFrame.CollisionTest
 {
     public interface IObserver
     {
-        void Update();
+        void Update(Vector2 vel, string type);
     }
 }
