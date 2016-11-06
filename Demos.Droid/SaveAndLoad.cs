c8479feb6d4b8fb561d0a8caf73e368edbc70737
AndrewWare95 <andware@gmail.com>
2016-11-06 00:53:18 +0000

    merge

---
+++ Demos.Droid/SaveAndLoad.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Android.Content.Res;
+using GameFrame.Services;
+
+namespace Demos.Droid
+{
+    public class SaveAndLoad : ISaveAndLoad
+    {
+        private AssetManager _assets;
+
+        public SaveAndLoad(AssetManager assets)
+        {
+            _assets = assets;
+        }
+
+        public void SaveText(string filename, string text)
+        {
+            var documentsPath = Environment.CurrentDirectory;
+            var filePath = Path.Combine(documentsPath, filename);
+            File.WriteAllText(filePath, text);
+        }
+
+        public string LoadText(string filename)
+        {
+            var toReturn = "";
+            using (var sr = new StreamReader(_assets.Open(filename)))
+            {
+                toReturn += sr.ReadToEnd();
+            }
+            return toReturn;
+        }
+    }
+}
---
+++ GameFrame.ContentPipeline/InkWriter.cs
@@ -0,0 +1,20 @@
+﻿using Ink.Runtime;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+namespace GameFrame.ContentPipeline
+{
+    [ContentTypeWriter]
+    public class InkWriter : ContentTypeWriter<string>
+    {
+        public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        {
+            return typeof(Story).AssemblyQualifiedName;
+        }
+
+        protected override void Write(ContentWriter output, string value)
+        {
+            output.Write(value);
+        }
+    }
+}
