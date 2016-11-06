d384605393cefe999773ba00beef58a1d8e149f9
Willieodwyer <willieod666@live.com>
2016-11-06 19:37:12 +0000

    Few moew tests

---
+++ GameFrame.Tests/Controller/ControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Demos.DesktopGl;
+using GameFrame.Controllers;
+using GameFrame.Controllers.GamePad;
+using GameFrame.Controllers.KeyBoard;
+using GameFrame.Controllers.SmartButton;
+using GameFrame.Movers;
+using GameFrame.PathFinding.Heuristics;
+using GameFrame.PathFinding.PossibleMovements;
+using GameFrame.ServiceLocator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameFrame.Tests.Controller
+{
+    [TestClass]
+    public class ControllerTest
+    {
+        public static IServiceLocator Instance;
+
+        [TestMethod]
+        public void SinglePLayerControllerFactoryTest()
+        {
+            StaticServiceLocator.AddService<IControllerSettings>(new ControllerSettings());
+            Instance = new ServiceLocator.ServiceLocator();
+            var settings = new ControllerSettings();
+            Instance.AddService<IControllerSettings>(settings);
+            Instance.GetService<IControllerSettings>();
+
+            var controller = new SinglePlayerControllerFactory();
+            controller.CreateEntityController(new BaseMovable(), new EightWayPossibleMovement(new CrowDistance()),
+                new MoverManager());
+            const Buttons buttons = Buttons.A;
+            controller.AddGamePadButton(new List<IButtonAble>(), buttons);
+            controller.AddKeyBoardButton(new List<IButtonAble>(), Keys.A);
+
+            var smart = new SmartController();
+            smart.AddButton(new BaseSmartButton(new KeyButton(Keys.A)));
+            smart.Update(new GameTime());
+        }
+    }
+}
--- GameFrame.Tests/Paths/PathTest.cs
+++ GameFrame.Tests/Paths/PathTest.cs
@@ -1,18 +1,62 @@
-﻿using GameFrame.Paths;
+﻿using GameFrame.CollisionSystems;
+using GameFrame.CollisionSystems.SpatialHash;
+using GameFrame.Movers;
+using GameFrame.PathFinding;
+using GameFrame.PathFinding.Heuristics;
+using GameFrame.PathFinding.PossibleMovements;
+using GameFrame.Paths;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended.Entities;
 using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
 
 namespace GameFrame.Tests.Paths
 {
     [TestClass]
     public class PathTest
     {
-        public AbstractPath Path;
 
         [TestMethod]
-        public void PathsTest()
+        public void PathsFinderTest()
         {
-            Path = new FinitePath();
+            var entity = new BaseMovable();
+            var endPoint = new Point();
+            
+            var collisionSystem = new CompositeAbstractCollisionSystem(new EightWayPossibleMovement(new CrowDistance()));
+            var tileSize = new Point();
+            var searchParams = new SearchParameters(entity.Position.ToPoint(), endPoint, collisionSystem, new Rectangle(new Point(), tileSize));
+            var path = new AStarPathFinder(searchParams, new EightWayPossibleMovement(new CrowDistance()));
+            Assert.AreNotSame(path.ClosedNodes, path.MapNodes);
+        }
+
+        [TestMethod]
+        public void PathTypesTest()
+        {
+            var eightWayPossibleMovement = new EightWayPossibleMovement(new CrowDistance());
+            PossibleMovementWrapper wrapper = new PossibleMovementWrapper(eightWayPossibleMovement);
+            Assert.AreEqual(wrapper.Heuristic,eightWayPossibleMovement.Heuristic);
+
+            eightWayPossibleMovement = new EightWayPossibleMovement(new DiagonalDistance());
+            wrapper = new PossibleMovementWrapper(eightWayPossibleMovement);
+            Assert.AreEqual(wrapper.Heuristic, eightWayPossibleMovement.Heuristic);
+
+            eightWayPossibleMovement = new EightWayPossibleMovement(new ManhattanDistance());
+            wrapper = new PossibleMovementWrapper(eightWayPossibleMovement);
+            Assert.AreEqual(wrapper.Heuristic, eightWayPossibleMovement.Heuristic);
+
+
+            var fourWayPossibleMovement = new FourWayPossibleMovement(new CrowDistance());
+            wrapper = new PossibleMovementWrapper(fourWayPossibleMovement);
+            Assert.AreEqual(wrapper.Heuristic, fourWayPossibleMovement.Heuristic);
+
+            fourWayPossibleMovement = new FourWayPossibleMovement(new DiagonalDistance());
+            wrapper = new PossibleMovementWrapper(fourWayPossibleMovement);
+            Assert.AreEqual(wrapper.Heuristic, fourWayPossibleMovement.Heuristic);
+
+            fourWayPossibleMovement = new FourWayPossibleMovement(new ManhattanDistance());
+            wrapper = new PossibleMovementWrapper(fourWayPossibleMovement);
+            Assert.AreEqual(wrapper.Heuristic, fourWayPossibleMovement.Heuristic);
         }
     }
 }
