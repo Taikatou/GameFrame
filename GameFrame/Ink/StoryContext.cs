6621a90aa446c1e5dc90fdb80eaf71738b322957
Taikatou <13123947@studentmail.ul.ie>
2016-11-07 18:48:34 +0000

    interceptor

---
+++ GameFrame/Ink/StoryContext.cs
@@ -0,0 +1,15 @@
+﻿using GameFrame.Interceptor;
+using Ink.Runtime;
+
+namespace GameFrame.Ink
+{
+    public class StoryContext : IContext
+    {
+        public string Text;
+
+        public StoryContext(Story story)
+        {
+            Text = story.currentText;
+        }
+    }
+}
