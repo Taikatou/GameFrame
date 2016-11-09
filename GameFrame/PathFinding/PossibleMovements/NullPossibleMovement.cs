b32596e98e6e9eb32e617cc81a55f6f91c6cdd23
cathal lenihan <cathallenihan@eircom.net>
2016-11-09 20:29:51 +0000

    Attempt at null object pattern

---
+++ GameFrame/PathFinding/PossibleMovements/NullPossibleMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using GameFrame.PathFinding.Heuristics;
+using Microsoft.Xna.Framework;
+
+namespace GameFrame.PathFinding.PossibleMovements
+{
+    class NullPossibleMovement : IPossibleMovements
+    {
+        public IHeuristic Heuristic { get; set; }
+
+        public NullPossibleMovement()
+        {
+            //do nothing
+            Debug.WriteLine("Null Pattern reached");
+        }
+
+        public NullPossibleMovement(IHeuristic heuristic)
+        {
+            //do nothing
+            Debug.WriteLine("Null Pattern reached");
+        }
+
+        public static IEnumerable<Point> FourWayAdjacentLocations(Point fromLocation)
+        {
+            return new[]
+            {
+                new Point(fromLocation.X-1, fromLocation.Y  ),
+                new Point(fromLocation.X,   fromLocation.Y+1),
+                new Point(fromLocation.X+1, fromLocation.Y  ),
+                new Point(fromLocation.X,   fromLocation.Y-1)
+            };
+        }
+
+        public IEnumerable<Point> GetAdjacentLocations(Point fromLocation)
+        {
+            return new[]
+            {
+                new Point(0, 0),
+                new Point(0, 0),
+                new Point(0, 0),
+                new Point(0, 0)
+            };
+        }
+
+        public IEnumerable<Point> PositionsToCheck(Point startPoint, Point endPoint)
+        {
+            return new[]
+            {
+                new Point(0, 0),
+                new Point(0, 0),
+                new Point(0, 0),
+                new Point(0, 0)
+            };
+        }
+    }
+}
