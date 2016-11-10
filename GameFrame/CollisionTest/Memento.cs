6a8d584a3eeb6469edbc402c179807f144570828
AndrewWare95 <andware@gmail.com>
2016-11-10 12:40:44 +0000

    Began implementation of Mumento design pattern

---
+++ GameFrame/CollisionTest/Memento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFrame.CollisionTest
+{
+    public class Memento
+    {
+
+        private BBObject obj;
+
+        public Memento(BBObject objCurrent)
+        {
+            obj = objCurrent;
+        }
+
+        public BBObject getSavedBBObject()
+        {
+            return obj;
+        }
+    }
+}
---
+++ GameFrame/CollisionTest/Originator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFrame.CollisionTest
+{
+    public class Originator
+    {
+
+        private BBObject obj;
+
+        public void set(BBObject newObj)
+        {
+            obj = newObj;
+        }
+
+        public Memento storeInMemento()
+        {
+            return new Memento(obj);
+        }
+
+        public BBObject restoreFromMemmento(Memento memento)
+        {
+            obj = memento.getSavedBBObject();
+            return obj;
+        }
+
+
+    }
+}
